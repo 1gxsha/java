5d82a2be44358355a97c5f52567816ec86a755aa
1gxsha <114615161+1gxsha@users.noreply.github.com>
2023-05-05 21:30:42 +0300

    Add files via upload

---
+++ ! EX 2/19/task2.java
@@ -0,0 +1,26 @@
+package num2test;
+
+import java.util.Scanner;
+
+public class task2 {
+    public static void main(String[] args) {
+        System.out.print("Enter a number: ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        System.out.println("Result: " + func(number));
+    }
+    public static int func(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        else if (n == 1) {
+            return 1;
+        }
+        else if (n % 2 == 0) {
+            return func(n / 2);
+        }
+        else {
+            return func((n - 1) / 2) + func((n - 1) / 2) + 1;
+        }
+    }
+}
---
+++ ! EX 2/23/task4.java
@@ -0,0 +1,27 @@
+package num2test;
+
+import java.util.Scanner;
+
+public class task4  {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a final number: ");
+        int n = scanner.nextInt();
+        System.out.print("Enter a first divider: ");
+        int x = scanner.nextInt();
+        System.out.print("Enter a second divider: ");
+        int y = scanner.nextInt();
+        System.out.println("\n" + "sum: " + result(n, x, y));
+        scanner.close();
+    }
+    public static int result(int m, int a, int b) {
+        int s = 0;
+        for (int i = 0; i <= m; i ++) {
+            System.out.print(i + " ");
+            if ((i % a == 0) || (i % b == 0)) {
+                s += i;
+            }
+        }
+        return s;
+    }
+}
